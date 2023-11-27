062beebff812255f1dc64120c4dd4e808177bc2e
Invariant64 <98544022+Invariant64@users.noreply.github.com>
2023-11-27 16:03:15 +0800

    fixed point

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,5 +1,7 @@
 /* Fixed point calculations. */
 
+typedef int fixed_point;
+
 /* Use a 32-bit signed integer to store a fixed point. 
    Fixed point in P.Q format. */
 #define FP_P 17
@@ -8,29 +10,66 @@
 #define FP_F (1 << FP_Q) 
 
 /* Convert integer to fixed point. */
-#define int_to_fp(n) ((n) * FP_F)
+fixed_point int_to_fp (int n)
+{
+  return n * FP_F;
+}
+
 /* Convert fixed point ti integer (rounding toward zero). */
-#define fp_to_int_0(x) ((x) / FP_F)
+fixed_point fp_to_int_0 (fixed_point x) 
+{
+  return x / FP_F;
+}
+
 /* Convert fixed point ti integer (rounding toward nearest). */
-#define fp_to_int_n(x) (((x) > 0) ? ((x + FP_F / 2) / FP_F) :\
-                                    ((x - FP_F / 2) / FP_F))
+fixed_point fp_to_int_n (fixed_point x)
+{ 
+  if (x > 0)
+    return (x + FP_F / 2) / FP_F;
+  else
+    return (x - FP_F / 2) / FP_F;
+}
 
 /* Caculations between a fixed point and a fixed point, 
    return a fixed point. */
-#define fp_add_fp(x, y) ((x) + (y))
-#define fp_sub_fp(x, y) ((x) - (y))
-#define fp_mul_fp (x, y) (((int64_t) (x)) * (y) / FP_F)
-#define fp_div_fp (x, y) (((int64_t) (x)) * FP_F / (y))
+fixed_point fp_add_fp (fixed_point x, fixed_point y)
+{
+  return x + y;
+}
+
+fixed_point fp_sub_fp (fixed_point x, fixed_point y)
+{
+  return x - y;
+}
+
+fixed_point fp_mul_fp (fixed_point x, fixed_point y)
+{
+  return (int64_t) x * y / FP_F;
+}
+
+fixed_point fp_div_fp (fixed_point x, fixed_point y)
+{
+  return (int64_t) x * FP_F / y;
+}
 
 /* Caculations between a fixed point and an integer, 
    return a fixed point. */
-#define fp_add_int(x, n) ((x) + (n) / FP_F)
-#define fp_sub_int(x, n) ((x) - (n) / FP_F)
-#define fp_mul_int (x, n) ((x) * (n))
-#define fp_div_int (x, n) ((x) / (n))
+fixed_point fp_add_int (fixed_point x, int n)
+{
+  return x + n / FP_F;
+}
 
+fixed_point fp_sub_int (fixed_point x, int n)
+{
+  return x - n / FP_F;
+}
 
-              
+fixed_point fp_mul_int (fixed_point x, int n)
+{
+  return x * n;
+}
 
-
-
+fixed_point fp_div_int (fixed_point x, int n)
+{
+  return x / n;
+}
